8bf4400e9c461295a0a799d4ec94425cc28c6b0e
rameshshikha <222shikhap@gmail.com>
2023-12-19 12:29:38 +0530

    push

---
+++ practicejenkins/src/test/java/demo/Parameterization.java
@@ -0,0 +1,12 @@
+package demo;
+
+public class Parameterization {
+
+             public void m1() {
+            	 String BROWSER = System.getProperty("browser");
+            	 String URL = System.getProperty("url");
+            	 System.out.println(BROWSER);
+            	 System.out.println(URL);
+            	 }
+
+}
